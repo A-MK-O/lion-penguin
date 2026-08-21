5233919ebef7d8b8219c5be004d85a0b5c0855af
A-MK-O <mk11224455@gmail.com>
2026-08-21 14:07:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const world = document.querySelector("#world");
 
+const pets = {
+  lion: document.querySelector("#lion"),
+  penguin: document.querySelector("#penguin")
+};
+
+let selectedPet = "lion";
+
+const roamTimers = {};
+const busyPets = new Set();
