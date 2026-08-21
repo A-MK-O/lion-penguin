86828381e434fc0f9994f5441996a96e6af08b03
A-MK-O <mk11224455@gmail.com>
2026-08-21 14:07:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,42 @@ let selectedPet = "lion";
 
 const roamTimers = {};
 const busyPets = new Set();
+
+function movePetRandomly(petName) {
+
+  if (busyPets.has(petName)) {
+    return;
+  }
+
+  const pet = pets[petName];
+
+  const maxX =
+    world.clientWidth - pet.offsetWidth;
+
+  const maxY =
+    world.clientHeight - pet.offsetHeight;
+
+
+  const randomX =
+    Math.random() * maxX;
+
+  const randomY =
+    Math.random() * maxY;
+
+
+  const travelTime =
+    2000 + Math.random() * 2500;
+
+
+  pet.style.transitionDuration =
+    `${travelTime}ms`;
+
+  pet.style.transform =
+    `translate(${randomX}px, ${randomY}px)`;
+
+
+  roamTimers[petName] = setTimeout(
+    () => movePetRandomly(petName),
+    travelTime + 1000 + Math.random() * 2000
+  );
+}
