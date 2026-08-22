8f3c6e06bfc8bde4602a343e11080ac96406eccd
A-MK-O <mk11224455@gmail.com>
2026-08-22 03:14:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+console.log("APP STARTED");
+document.body.dataset.appLoaded = "yes";
+
 // ==================================================
 // BASIC ELEMENTS
 // ==================================================
